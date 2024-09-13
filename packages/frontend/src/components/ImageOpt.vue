fb12021959434cf03130d4fe52c7c03cc08ded3a
Ken Snyder <ken@ken.net>
2024-09-13 15:12:03 -0700

    feat: added overlay functionality with default slot

--- packages/frontend/src/components/ImageOpt.vue
+++ packages/frontend/src/components/ImageOpt.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, defineProps } from "vue";
+import { ref, computed, defineProps, useAttrs, useSlots } from "vue";
 import {
     CssObjectFit,
     CssAspectRatio,
@@ -27,6 +27,12 @@ const primaryLoaded = () => {
     primary.value = true;
 };
 
+type OverlayPosition = "left" | "right" | "top" | "bottom";
+
+const slots = useSlots();
+
+const hasSlotContent = computed(() => typeof slots.default === "function" && slots.default().length>0 ? true : false) 
+
 const {
     // @ts-ignore
     src,
@@ -41,6 +47,10 @@ const {
     width,
     height,
     minHeight,
+    overlayPosition = "bottom",
+    overlayBgColor = "transparent",
+    overlayBgOpacity = "0.9",
+    overlayBlur = "55px"
 } = defineProps<{
     src: string;
     alt?: string;
@@ -54,11 +64,16 @@ const {
     height?: CssSizing;
     minHeight?: CssSizing;
     width?: CssSizing;
+    overlayPosition?: OverlayPosition;
+    overlayBgColor?: CssColor;
+    overlayBgOpacity?: number | `${number}`;
+    overlayBlur?: CssSizing;
 }>();
 
 const noExt = (img: string) => {
     return img.includes(".") ? img.split(/.*\./).slice(0, -1).join("") : img;
 };
+
 
 const srcset = (format: string) =>
     computed(() => {
@@ -66,68 +81,91 @@ const srcset = (format: string) =>
             .map((s) => `${noExt(src)}-${s}.${format} ${s}w`)
             .join(", ");
     });
+
+
+const a = ref(useAttrs());
+const borderRadius = computed(() => {
+    const rounded = Object.keys(a.value).filter(i => i.startsWith("rounded"));
+    
+    return rounded.includes("rounded-full")
+        ? "9999px"
+        : rounded.includes("rounded-3xl")
+          ? "24px"
+          : rounded.includes("rounded-2xl")
+            ? "16px"
+            : rounded.includes("rounded-xl")
+              ? "12px"
+              : rounded.includes("rounded-lg")
+                ? "8px"
+                : rounded.includes("rounded-md")
+                  ? "6px"
+                  : rounded.includes("rounded-sm")
+                    ? "2px"
+                    : rounded.includes("rounded") && typeof rounded.find(i => i == "rounded") === "number"
+                          ? `${rounded.find(i => i == "rounded")}px`
+                          : rounded.includes("rounded") ? "4px"
+                      : "0px";
+});
+
+if(height === undefined && aspectRatio === undefined) {
+    console.warn(`The image "${src}" is being displayed with ImageOpt component but does not express either a height or an aspect ratio! This will lead to decreased rendering performance and/or unexpected results.`)
+}
+if(height !== undefined && aspectRatio !== undefined) {
+    console.warn(`The image "${src}" is being displayed with ImageOpt component but it has BOTH the height and aspect-ratio set! Use one or the other but not both.`)
+}
 </script>
 
 <template>
-    <div class="image-opt" ref="el">
-        <div class="color-backing" :class="{ replaced: blur }"></div>
-        <img
-            class="blurry-leader"
-            :class="{ replaced: primary }"
-            :src="`${noExt(src)}-blurred.jpg`"
-            :alt="alt"
-            loading="eager"
-            @load="blurLoaded"
-        />
-
-        <picture v-if="blur" class="primary-image" :class="{ ready: primary }">
-            <source
-                v-for="format in formats"
-                :key="format"
-                :type="`image/${format}`"
-                :srcset="`${srcset(format).value}`"
-                :sizes="`${vw}vw`"
-            />
-
-            <!-- FALLBACK IMAGE -->
+    <div class="image-opt" ref="el" :data-border="borderRadius">
+        <div class="wrapper">
+            <div class="color-backing" :class="{ replaced: blur }"></div>
             <img
-                :src="src"
+                class="blurry-leader"
+                :class="{ replaced: primary }"
+                :src="`${noExt(src)}-blurred.jpg`"
                 :alt="alt"
-                :loading="loading"
-                @load="primaryLoaded"
+                loading="eager"
+                @load="blurLoaded"
             />
-        </picture>
+            <picture v-if="blur" class="primary-image" :class="{ ready: primary }">
+                <source
+                    v-for="format in formats"
+                    :key="format"
+                    :type="`image/${format}`"
+                    :srcset="`${srcset(format).value}`"
+                    :sizes="`${vw}vw`"
+                />
+                <!-- FALLBACK IMAGE -->
+                <img
+                    :src="src"
+                    :alt="alt"
+                    :loading="loading"
+                    @load="primaryLoaded"
+                />
+            </picture>
+            <div v-if="hasSlotContent" class="overlay" :class="overlayPosition">
+                <div class="overlay-content" :class="overlayPosition">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="css" scoped>
-@property --image-width {
-    syntax: "<size>";
-    inherit: true;
-    default: v-bind(width);
-}
-@property --image-height {
-    syntax: "<size>";
-    inherit: true;
-    default: v-bind(height);
-}
-@property --image-min-height {
-    syntax: "<size>";
-    inherit: true;
-    default: v-bind(minHeight);
-}
 
-:root {
-    --image-width: v-bind(width);
-    --image-height: v-bind(height);
-    --aspect-ratio: v-bind(aspectRatio);
-}
-
-.image-opt {
-    display: block;
+.image-opt .wrapper {
+    display: grid;
     position: relative;
     width: 100%;
-    aspect-ratio: var(--aspect-ratio);
+    width: v-bind(width);
+    height: v-bind(height);
+    min-height: v-bind(minHeight);
+    padding: 0;
+    margin: 0;
+    aspect-ratio: v-bind(aspectRatio);
+    border-radius: v-bind("borderRadius");
+    overflow: hidden;
 }
 .color-backing {
     background-color: v-bind(color);
@@ -144,8 +182,9 @@ const srcset = (format: string) =>
     position: absolute;
     inset: 0 0 0 0;
     width: 100%;
-    height: auto;
+    height: v-bind(height);
     aspect-ratio: v-bind(aspectRatio);
+
     object-fit: v-bind(fit);
     object-position: v-bind(position);
     overflow: hidden;
@@ -159,13 +198,8 @@ const srcset = (format: string) =>
 .primary-image {
     position: absolute;
     inset: 0 0 0 0;
-    width: 100%;
     z-index: 5;
     opacity: 0;
-    overflow: hidden;
-    aspect-ratio: v-bind(aspectRatio);
-    object-fit: v-bind(fit);
-    object-position: v-bind(position);
 }
 .primary-image.ready {
     transition: opacity 600ms ease-in;
@@ -173,8 +207,46 @@ const srcset = (format: string) =>
 }
 
 .primary-image img {
+    width: 100%;
+    height: v-bind(height);
     aspect-ratio: v-bind(aspectRatio);
+
     object-fit: v-bind(fit);
     object-position: v-bind(position);
+}
+
+.overlay {
+    display: grid;
+    position: absolute;
+    z-index: 6;
+    padding: 1rem;
+}
+
+.overlay::after {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: v-bind("overlayBgOpacity");
+    background-color: v-bind("overlayBgColor");
+    content: "";
+    backdrop-filter: blur(v-bind("overlayBlur"));
+    z-index: -1;
+}
+
+.overlay.bottom {
+    bottom: 0;
+    width: 100%;
+}
+.overlay.top {
+    top: 0;
+    width: 100%;
+}
+.overlay.left {
+    left: 0;
+    height: 100%;
+}
+.overlay.right {
+    right: 0;
+    height: 100%;
 }
 </style>
